cb989fd0f3cc26c00fb1eb7f7711f8cee69d3f1a
rubal501 <rubal820@gmail.com>
2017-07-21 19:38:54 -0500

    mejoras en el index.html2

---
+++ programs/js/template_generator/index_generator.js
@@ -0,0 +1,33 @@
+/**
+ * Created by rubal501 on 7/21/17.
+ */
+$(function() {
+    $.get('templates/carousel_template.mst', function (template) {
+        $.getJSON('data/carousel_data.json', function(data) {
+            var html = Mustache.to_html(template,data);
+            $('#seccion1').html(html);
+
+        });
+    });
+    $.get('templates/academia_template.mst', function (template) {
+        $.getJSON('data/academia_data.json', function(data) {
+            var html = Mustache.to_html(template,data);
+            $('#academia').html(html);
+
+        });
+    });
+    $.get('templates/teacher_template.mst', function (template) {
+        $.getJSON('data/teachers_data.json', function(data) {
+            var html = Mustache.to_html(template,data);
+            $('#maestrosList').html(html);
+
+        });
+    });
+    $.get('templates/footer_template.mst', function (template) {
+        $.getJSON('data/footer_data.json', function(data) {
+            var html = Mustache.to_html(template,data);
+            $('#footer-zone').html(html);
+
+        });
+    });
+});
